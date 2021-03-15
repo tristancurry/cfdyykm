6d2afe2d67530e00b8bec37026d480b11b2a5892
Tristan Miller <tristan.j.miller@gmail.com>
2021-03-15 20:49:25 +1030

    Update devochka.js

--- fluid_simulation_early2021/devochka.js
+++ fluid_simulation_early2021/devochka.js
@@ -5,10 +5,12 @@ const ELM_DIAM = 0.064; //m
 
 const dt = 0.1; //s
 
-const INTERVALS = 100;
+let INTERVALS = 1;
+if (INTERVALS < 1) {INTERVALS = 1;}
 const dt_sub = dt/INTERVALS;
+console.log(dt_sub);
 
-const PRESSURE_W = 10e5; //Pa
+const PRESSURE_W = 1e5; //Pa
 
 let canvas0 = document.getElementById('canvas0');
 let ctx0 = canvas0.getContext('2d');
@@ -19,19 +21,11 @@ let pipe_nodes = Math.floor(pipe_length/ELM_LENGTH);
 pipe_length = pipe_nodes*ELM_LENGTH;
 
 let initialiseGrid = (value) => {
-  if(!Array.isArray(value)) {
-    let array = new Float64Array(pipe_nodes);
-    for (let j = 0; j < pipe_nodes; j++) {
-      if (!value) {array[j] = 0;} else {array[j] = value;}
-    }
-    return array;
-  } else {
-    let array = [];
-    for (let j = 0; j < pipe_nodes; j++) {
-      if (!value) {array.push(0);} else {array.push(value);}
-    }
-    return array;
+  let array = [];
+  for (let j = 0; j < pipe_nodes; j++) {
+    if (!value) {array.push(0);} else {array.push(value);}
   }
+  return array;
 }
 
 //the following quantities will be packaged together for each pipe
@@ -43,7 +37,7 @@ let v = {
   targets: initialiseGrid([]),
 }
 
-v.values[3] = 10;
+v.values[3] = 5;
 
 let rho = {
   values: initialiseGrid(RHO_W),
@@ -64,6 +58,8 @@ let mass = {
   unsigned: true,
 }
 
+mass.values[3] = 3;
+
 let pressure = {
   values: initialiseGrid(default_mass),
   values_new: initialiseGrid(),
@@ -82,13 +78,13 @@ let temp = {
 
 //
 
-let advect_fwd = (quantity, vel, delta_t) => {
+let advect_fwd = (quantity, velocity, delta_t) => {
 
   quantity.targets = initialiseGrid([]);
   //loop through all nodes in vector field and advect quantities to new locations.
-  for (let i = 0, l = vel.length; i < l; i++) {
-    if (vel[i] != 0) {
-      let vel = vel[i];
+  for (let i = 0, l = velocity.values.length; i < l; i++) {
+    if (velocity.values[i] != 0) {
+      let vel = velocity.values[i];
       let dist = vel*delta_t;
 
       let nodes = dist/ELM_LENGTH; //how many nodes have been traversed?
@@ -106,108 +102,79 @@ let advect_fwd = (quantity, vel, delta_t) => {
       }
 
       let target_nodes = [];
+
       for (let u = 0; u < neighbours.length; u++) {
-          target_nodes.push({x:neighbours[u]});
+          //find how far away the target node is from the effective node and use to apportion the advected quantity
+          let frac = 1 - Math.abs(node_res - neighbours[u]);
+          //check if target is 'out of bounds' and handle appropriately.
+          //This would ordinarily mean checking downstream connected pipes and seeing where in those it ends up
+          //If there's a wall instead, simply move the fluid as far as possible and then handling reflection/attenuation later
+          if (neighbours[u] >= l) { neighbours[u] = l - 1; }
+          else if (neighbours[u] < 0) { neighbours[u] = 0; }
+          target_nodes.push({x:neighbours[u], fraction: frac});
       }
-
-      //now apportion the advected quantity according to neighbours' relative distance from the effective node
-      for (let j = 0; j < target.nodes)
-
-
+      quantity.targets[i] = target_nodes.slice();
     }
-    for (let j = 0, m = x_comps[i].length; j < m; j++) {
-      if(x_comps[i][j] != 0 || y_comps[i][j] != 0) {
-        let dist_x = x_comps[i][j]*dt/dx;
-        let dist_y = y_comps[i][j]*dt/dy;
-        //next - determine the nodes this (off-grid) location falls between
-        //i.e. resultant i =  i + dist_x (floor and ceiling), similarly for resultant j
-        let i_res = i + dist_x;
-        let i_res_neighbours = [Math.floor(i_res), Math.ceil(i_res)];
-        if (i_res_neighbours[0] == i_res_neighbours[1]) {
-          i_res_neighbours = [i_res];
-        }
+  }
+}
 
-        let j_res = j + dist_y;
-        let j_res_neighbours = [Math.floor(j_res), Math.ceil(j_res)];
-        if (j_res_neighbours[0] == j_res_neighbours[1]) {
-          j_res_neighbours = [j_res];
-        }
-
-
-        let target_nodes = [];
-        for (let u = 0; u < i_res_neighbours.length; u++) {
-          for (let v = 0; v < j_res_neighbours.length; v++) {
-            target_nodes.push({x:i_res_neighbours[u], y:j_res_neighbours[v]});
-          }
-        }
-
-        //then 'reverse-interpolate' in i and j to work out how much of the quantity is received by each node
-        let dist_sum = 0;
-        if(target_nodes.length > 1) {
-          for (let k = 0; k < target_nodes.length; k++) {
-            //calculate distances of each node from the resultant point
-            let target = target_nodes[k];
-            target.dist = 1;// Math.sqrt(Math.pow(target.x - i_res, 2) + Math.pow(target.y - j_res, 2));
-            dist_sum += 1/target.dist;
-
-          }
-
-          for (let k = 0; k < target_nodes.length; k++) {
-            let target = target_nodes[k];
-            target.fraction = (1 - Math.abs(i_res - target.x)/dx)*(1 - Math.abs(j_res - target.y)/dy);
-          }
-        } else {
-          target_nodes[0].fraction = 1;
-        }
-
-        //check for out-of-bounds targets and wrap or reflect as necessary.
-        for (let k = 0; k < target_nodes.length; k++) {
-          let target = target_nodes[k];
-
-          if (target.x > nx - 1) {
-            target.x = (target.x)%nx;
-            if (wrap_x == false) {
-              target.x = nx - 1 - target.x;
-            }
-          } else if (target.x < 0) {
-            if(wrap_x == true) {
-              while (target.x < 0) {
-                target.x  = nx + target.x;
-              }
-            } else {
-              while(target.x < -1*nx) {
-                target.x += nx;
-              }
-              target.x = -1*target.x;
-            }
-          }
-
-          if (target.y > ny - 1) {
-            target.y = (target.y)%ny;
-            if (wrap_y == false) {
-              target.y = ny - 1 - target.y;
-            }
-          } else if (target.y < 0) {
-            if (wrap_y == true) {
-              while (target.y < 0) {
-                target.y = ny + target.y;
-              }
-            } else {
-              while(target.y < -1*ny) {
-                target.y += ny;
-              }
-              target.y = -1*target.y;
-            }
-          }
-        }
-
-        //store the fractions requested by each point for later application
-        quantity.targets[i][j] = target_nodes.slice();
+//this function checks each point for the amount of a quantity requested
+//this is only important if it's an unsigned quantity like mass
+//go to each node. Add up the the requested proportions.
+//if the proportion is > 1, then normalise the outflows.
+//mutate the targets list as we go (no need for a deep copy)
+let restrictOutflow = (targets) => {
+  for (let i = 0, l = targets.length; i < l; i++) {
+    let outflows = 0;
+    let thisTargets = targets[i];
+    for(let j = 0, m = thisTargets.length; j < m; j++) {
+      outflows += thisTargets[j].fraction;
+    }
+    if (outflows > 1) {
+      //normalise the outflows
+      for(let j = 0, m = thisTargets.length; j < m; j++) {
+        thisTargets[j].fraction = thisTargets[j].fraction/outflows;
       }
     }
   }
 }
 
+let applyFlows = (quantity) => {
+  if (quantity.unsigned) {
+    restrictOutflow(quantity.targets);
+  }
+
+  //seed quantity.values_new matrix with current values
+  //the sub arrays must be cloned, rather than referenced
+  for (let i = 0, l = quantity.values.length; i < l; i++) {
+    if (quantity.values_old) {
+      quantity.values_old = quantity.values.map(elem => elem);
+    }
+    quantity.values_new = quantity.values.map(elem => elem);
+  }
+
+  for (let i = 0, l = quantity.targets.length; i < l; i++) {
+    let thisTargets = quantity.targets[i];
+    let thisValue = quantity.values[i];
+    for (let j = 0, m = thisTargets.length; j < m; j++) {
+      let thisTarget = thisTargets[j];
+
+      let flow = thisTarget.fraction*thisValue;
+      if(!quantity.unsigned) {
+        if((flow > 0 && i == l - 1) || (flow < 0 && i == 0)) {
+          quantity.values_new[thisTarget.x] -= flow;
+        }
+      } else {
+        quantity.values_new[thisTarget.x] += flow;
+      }
+      quantity.values_new[i] -= flow;
+    }
+  }
+
+  //update quantity vector with new values
+  //the array must be cloned, rather than referenced
+  quantity.values = quantity.values_new.map(elem => elem);
+}
 
 //
 let render = () => {
@@ -222,7 +189,13 @@ let render = () => {
 
 let animate = () => {
   for (let i = 0; i < INTERVALS; i++) {
+    mass.targets = initialiseGrid([]);
+    v.targets = initialiseGrid([]);
 
+    advect_fwd(mass, v, dt_sub);
+    advect_fwd(v, v, dt_sub);
+    applyFlows(mass);
+    applyFlows(v);
   }
 
   render();
